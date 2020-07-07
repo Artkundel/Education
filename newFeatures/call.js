a2334154ec71763123f2dbeafa517166af3449e0
Arthur <arthurkundel@gmail.com>
2020-07-07 23:36:39 +0700

    lessons24

---
+++ newFeatures/call.js
@@ -0,0 +1,26 @@
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+
+  if (price < 0) {
+    throw RangeError('Нельзя создать продукт ' +
+                      this.name + ' с отрицательной ценой');
+  }
+}
+
+function Food(name, price) {
+  Product.call(this, name, price);
+  this.category = 'еда';
+}
+
+Food.prototype = Object.create(Product.prototype);
+
+function Toy(name, price) {
+  Product.call(this, name, price);
+  this.category = 'игрушка';
+}
+
+Toy.prototype = Object.create(Product.prototype);
+
+var cheese = new Food('фета', 5);
+var fun = new Toy('робот', 40);
